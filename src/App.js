a1899f6bce2774fef4bc1f807dd123350bb12dcb
Rohit Sriram <rohitsram10@outlook.com>
2025-06-12 01:24:52 -0400

    intitial repo creation and basic UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$300<span>.00</span></h1>
+      <form>
+        <div className='basics'>
+          <input type='text' placeholder={'+350 apple watch'}/>
+          <input type='datetime-local'/>
+        </div>
+        <div className='description'>
+          <input type='text' placeholder={'description'}/>
+        </div>
+        <button type='submit'>Add new transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>Free-lance gig</div>
+            <div className='description'>Time for a change</div>
+          </div>
+          <div className='right'>
+            <div className='price green'>+$300</div>
+            <div className='datetime'>2024-10-23 15:40</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='description'>Time for a change</div>
+          </div>
+          <div className='right'>
+            <div className='price red'>-$500</div>
+            <div className='datetime'>2024-10-23 15:40</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>gig 2</div>
+            <div className='description'>Time for a change</div>
+          </div>
+          <div className='right'>
+            <div className='price green'>+$200</div>
+            <div className='datetime'>2024-10-23 15:40</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
